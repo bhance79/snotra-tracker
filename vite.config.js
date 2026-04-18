a2c2e7519dd1e84e6bea3a00dda374bf5b4423a6
Ivan <ivanr1520@gmail.com>
2026-04-18 09:40:46 -0700

    stable release; updated logo and added some features to edit recent activities

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    hmr: {
-      host: 'localhost',
-      protocol: 'ws',
-    },
+    host: true,
+    hmr: true,
   },
 })
